37e868950e74806772036c78410f322ad863ed1d
DenisJang <deniijang@gmail.com>
2021-10-11 21:29:42 -0300

    primer

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app=express();
+const path=require("path");
+
+
+
+app.listen(4000, () => 
+    console.log("esto fue exitoso")
+);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "views/home.html"))
+});
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"))
+});
+app.get("/ingresa", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/ingresa.html"))
+});
+app.post("/register", (req, res) => {
+    res.redirect("/")
+});
+
+
+app.use(express.static("public"));
